52a07f10900f0ed62c0c04e7fc627ab25c939158
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-20 18:04:05 -0500

    Created local secrets.yml; fixed typo in team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ActiveRecord::Base
 
   has_many :recommendations
-  has_many :examples, through :recommendations
+  has_many :examples, through: :recommendations
   has_many :users
   
 end
