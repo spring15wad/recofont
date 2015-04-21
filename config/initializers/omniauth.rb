4469c5b62fa0021b8e869151e955d9dd3bd9b3c5
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-20 19:11:25 -0500

    omniauth.rb defines :provider and loads keys!

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,16 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+
+# For example, in MJF's ~/.profile, there is:
+#
+#  # -- Needed for RecoFont Project -- #
+#  export GH_ID="3%%%%%%%%%%%%%%%%%%e"
+#  export GH_Secret="49%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%4c"
+#
+# (without having %'s masking characters, of course)
+# This loads the keys into environment variables, which
+# then get loaded into variables in the Rails app 
+# by the following…
+
+use OmniAuth::Builder do
+  provider :github, ENV['GH_ID'], ENV['GH_Secret']
+end
