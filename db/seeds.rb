607a5e46b44ba7b3f7d3fc1abc899eb58e4b8106
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-24 15:25:38 -0500

    $ rake db:migrate ; rake db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
 Example.create!([
-  {url: "https://github.com/intridea/omniauth/blob/master/README.md", file: "", recommendation_id: 1, description: "Foo and Bar."}
+  {url: "none", file: "", recommendation_id: 1, description: "none"},
+  {url: "https://github.com/intridea/omniauth/blob/master/README.md", file: "", recommendation_id: 2, description: "Foo and Bar."}
 ])
 Recommendation.create!([
-  {recommendation: "", font_stack: "font-family: Arial, \"Helvetica Neue\", Helvetica, \"Liberation Sans\", \"DejaVu Sans Condensed\", sans-serif;", pull_request_url: "https://github.com/spring15wad/recofont/pulls", team_id: 1}
+  {recommendation: "none", font_stack: "none", pull_request_url: "none", team_id: 1},
+  {recommendation: "", font_stack: "font-family: Arial, \"Helvetica Neue\", Helvetica, \"Liberation Sans\", \"DejaVu Sans Condensed\", sans-serif;", pull_request_url: "https://github.com/spring15wad/recofont/pulls", team_id: 2}
 ])
 Team.create!([
-  {project_name: "RollingRead", proposal: "https://github.com/spring15wad/metawad/blob/master/beta-app-proposal.md", github_repo: "https://github.com/spring15wad/rollingread"},
+  {project_name: "default", proposal: "none", github_repo: "none"},
   {project_name: "RollingRead", proposal: "https://github.com/spring15wad/metawad/blob/master/beta-app-proposal.md", github_repo: "https://github.com/spring15wad/rollingread"}
+])
+User.create!([
+  {username: "mfreema5", uid: "5863302", provider: "github", team_id: 2}
 ])
