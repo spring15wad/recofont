8a947fc09803b1281330f88cea6b9bf9dad8ef47
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-24 16:24:14 -0500

    Forgot one comma in db/seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ Team.create!([
   {project_name: "RollingRead", proposal: "https://github.com/spring15wad/metawad/blob/master/beta-app-proposal.md", github_repo: "https://github.com/spring15wad/rollingread"}
 ])
 User.create!([
-  {username: "mfreema5", uid: "5863302", provider: "github", team_id: 2}
+  {username: "mfreema5", uid: "5863302", provider: "github", team_id: 2},
   {username: "Dan", uid: "8630916", provider: "github", team_id: 2}
 ])
