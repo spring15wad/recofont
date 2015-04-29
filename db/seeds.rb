bd8d54273292eacbb4bcb4d2d2c18e4c12e7e4c2
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-29 17:12:39 -0500

    Commas in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 Team.create!([
-  {project_name: "default", proposal: "none", github_repo: "none"},
+  {project_name: "default", proposal: "none", github_repo: "none"}
 ])
 Recommendation.create!([
-  {recommendation: "none", font_stack: "none", pull_request_url: "none", team_id: 1},
+  {recommendation: "none", font_stack: "none", pull_request_url: "none", team_id: 1}
 ])
 Example.create!([
-  {description: "none", url: "https://example.com", recommendation_id: 1},
+  {description: "none", url: "https://example.com", recommendation_id: 1}
 ])
 Attachment.create!([
   {filename: "NA" , mime_type: "image/png", example_id: 1}
