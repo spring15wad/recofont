3dd922aac0fb3178d916a9b0476d00fb9e2402cc
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-30 10:45:21 -0500

    All blank pull-request URL field.

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -3,6 +3,6 @@ class Recommendation < ActiveRecord::Base
   has_one :team
   has_many :example
 
-  validates :pull_request_url, format: { with: /\Ahttp/, message: ' – A valid URL you must enter.' }
+  validates :pull_request_url, { allow_blank: "true", format: { with: /\Ahttp/, message: " – A valid URL you must enter." } }
 
 end
