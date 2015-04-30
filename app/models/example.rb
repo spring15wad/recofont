866d2ab10ef5046160e178c5e35a6e6c979eae95
Mike Freeman <mfreema5@hawk.iit.edu>
2015-04-30 06:52:52 -0500

    Add simplistic validations for URLs.

--- app/models/example.rb
+++ app/models/example.rb
@@ -4,4 +4,7 @@ class Example < ActiveRecord::Base
   belongs_to :team
   has_one :attachment
 
+  validates :url, format: { with: /\Ahttp/, message: ' – A valid URL you must enter.' }
+
+
 end
--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -3,4 +3,6 @@ class Recommendation < ActiveRecord::Base
   has_one :team
   has_many :example
 
+  validates :pull_request_url, format: { with: /\Ahttp/, message: ' – A valid URL you must enter.' }
+
 end
