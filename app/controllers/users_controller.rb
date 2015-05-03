d937a4de3068a516d8857b9c10e23e40d8f8c4a8
Mike Freeman <mfreema5@hawk.iit.edu>
2015-05-03 05:35:21 -0500

    Remove pseudo-security from ‘users_controller.rb’ May prevent changing teams.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :allow_user, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
